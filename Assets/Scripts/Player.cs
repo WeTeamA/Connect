789e1f4e00a7718068fca99079818f605dfc9884
SirotenkoMax <sirotenko.max1998@gmail.com>
2020-03-20 23:20:31 +0300

    Build Ready

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -160,67 +160,53 @@ public class Player : MonoBehaviour
             {
                 if (touch.phase == TouchPhase.Began)
                 {
-                    // Construct a ray from the current touch coordinates
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    if (Physics.Raycast(ray))
-                    {
-                        LastFrameDist = closestHookTo.HookToObject.DistanceToPlayer; //for attach calculations
-                    }
+                    LastFrameDist = closestHookTo.HookToObject.DistanceToPlayer; //for attach calculations
+
                 }
                 if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
                 {
-                    // Construct a ray from the current touch coordinates
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    if (Physics.Raycast(ray))
+                    GetComponent<ConstantForce>().torque = Vector3.zero; //stops rotating
+                    if (gameObject.GetComponent<HingeJoint>())
                     {
-                        GetComponent<ConstantForce>().torque = Vector3.zero; //stops rotating
-                        if (gameObject.GetComponent<HingeJoint>())
-                        {
-                            Destroy(gameObject.GetComponent<HingeJoint>()); //destriys connections
-                        }
+                        Destroy(gameObject.GetComponent<HingeJoint>()); //destriys connections
                     }
                 }
                 if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
                 {
-                    // Construct a ray from the current touch coordinates
-                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    if (Physics.Raycast(ray))
+                    if (closestHookTo.HookToObject.DistanceToPlayer > LastFrameDist) //if player moves far away from hookTO
                     {
-                        if (closestHookTo.HookToObject.DistanceToPlayer > LastFrameDist) //if player moves far away from hookTO
+                        if (!gameObject.GetComponent<HingeJoint>())
                         {
-                            if (!gameObject.GetComponent<HingeJoint>())
+                            if (playerStats.AngleToHookTo < 10 && playerStats.AngleToHookTo > -10)
                             {
-                                if (playerStats.AngleToHookTo < 10 && playerStats.AngleToHookTo > -10)
+                                gameObject.AddComponent<HingeJoint>().anchor = transform.InverseTransformPoint(closestHookTo.transform.position); // when to attach
+                            }
+                            else
+                            {
+
+                                if (playerStats.AngleX > 0)
                                 {
-                                    gameObject.AddComponent<HingeJoint>().anchor = transform.InverseTransformPoint(closestHookTo.transform.position); // when to attach
+                                    GetComponent<ConstantForce>().torque = new Vector3(playerStats.RotationForce, 0, 0); // по часовой
+                                    GetComponent<ConstantForce>().force += lookAtObj.transform.forward * playerStats.MoveAccelerateMultiply;
                                 }
                                 else
                                 {
-
-                                    if (playerStats.AngleX > 0)
-                                    {
-                                        GetComponent<ConstantForce>().torque = new Vector3(playerStats.RotationForce, 0, 0); // по часовой
-                                        GetComponent<ConstantForce>().force += lookAtObj.transform.forward * playerStats.MoveAccelerateMultiply;
-                                    }
-                                    else
-                                    {
-                                        GetComponent<ConstantForce>().torque = new Vector3(-playerStats.RotationForce, 0, 0); // против часовой
-                                        GetComponent<ConstantForce>().force += lookAtObj.transform.forward * playerStats.MoveAccelerateMultiply;
-                                    }
+                                    GetComponent<ConstantForce>().torque = new Vector3(-playerStats.RotationForce, 0, 0); // против часовой
+                                    GetComponent<ConstantForce>().force += lookAtObj.transform.forward * playerStats.MoveAccelerateMultiply;
                                 }
                             }
                         }
-                        LastFrameDist = closestHookTo.HookToObject.DistanceToPlayer; //for attach calculations
+                    }
+                    LastFrameDist = closestHookTo.HookToObject.DistanceToPlayer; //for attach calculations
 
-                        if (gameObject.GetComponent<HingeJoint>())
-                        {
-                            GetComponent<ConstantForce>().force *= closestHookTo.HookToObject.acceleration; // Add force when attached
-                        }
+                    if (gameObject.GetComponent<HingeJoint>())
+                    {
+                        GetComponent<ConstantForce>().force *= closestHookTo.HookToObject.acceleration; // Add force when attached
+                    }
 
-                        if (GetComponent<HingeJoint>())
-                        {
-                            FindClosestObj(); //Searches when already attached
-                        }
+                    if (GetComponent<HingeJoint>())
+                    {
+                        FindClosestObj(); //Searches when already attached
                     }
                 }
             }
