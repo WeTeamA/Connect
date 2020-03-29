530b210b6c1ed169c6f7a289e80ec3e347d96aa9
Mikhail <tokkotilo@mail.ru>
2020-03-29 20:38:36 +0300

    Fix distruction script

--- Assets/Scripts/Mikhail/Distruction.cs
+++ Assets/Scripts/Mikhail/Distruction.cs
@@ -9,15 +9,17 @@ public class Distruction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Corner = GameObject.FindWithTag("ScreenCorner").transform.position; //Определяем угол экрана
-        Corner = Camera.main.transform.position;
-        Corner += new Vector3(-0.300f,-1f,0.65f); //Коррекция относительно камеры (камера не может быть под углом)
+
+
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        Corner = Camera.main.transform.position;
+        Corner += new Vector3(-2.01f, 1.43f, -1f); //Коррекция относительно камеры (камера не может быть под углом)
+
         if (gameObject.GetComponent<FixedJoint>() == null)
         {
             t += Time.deltaTime; //Считаем время после исчезновения FixedJoint
@@ -27,29 +29,12 @@ public class Distruction : MonoBehaviour
         if (t > 3)
         { 
             gameObject.GetComponent<MeshCollider>().isTrigger = true;
-            gameObject.GetComponent<Rigidbody>().AddForce((Corner - gameObject.transform.position) * 0.006f);
-            if (gameObject.transform.position.y > Camera.main.transform.position.y-0.2f || t>5)
+            gameObject.GetComponent<Rigidbody>().AddForce((Corner - gameObject.transform.position) * 0.1f);
+            if (gameObject.transform.position.x > Camera.main.transform.position.x-0.2f || t>5)
                  {
                      gameObject.SetActive(false);
                 //Тут дописать пульсацию UI счетчика очков
                  }
-        }
-
-        
-
-        //Следующий код надо будет удалить потом!!!!!!!
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-2,2), Random.Range(-2, 2), Random.Range(-2, 2)));
-        }
-
-        foreach (Touch touch in Input.touches)
-        {
-            if (touch.phase == TouchPhase.Began)
-            {
-                gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-2, 2), Random.Range(-2, 2), Random.Range(-2, 2)));
-
-            }
         }
     }
 }
