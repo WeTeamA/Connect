4ba8ac348bcb0aee393b6f42c87e5cba604cf249
Mikhail <tokkotilo@mail.ru>
2020-04-02 01:10:44 +0300

    Added comment to rotation script

--- Assets/ScriptsTest/ChangeSkyboxRotation.cs
+++ Assets/ScriptsTest/ChangeSkyboxRotation.cs
@@ -13,6 +13,6 @@ public class ChangeSkyboxRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Camera.main.transform.position.z);
+        RenderSettings.skybox.SetFloat("_Rotation", Camera.main.transform.position.z); // Меняет угол SkyBox'а на основе позиции первой камеры с тегом "MainCamera" по координате Z
     }
 }
