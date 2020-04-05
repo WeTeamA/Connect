f49275fbd7133290af4ff8c079db8747ad432f1d
Mikhail <tokkotilo@mail.ru>
2020-04-05 16:14:04 +0300

    Added method for Following script

--- Assets/Scripts/SimpleCameraFollow.cs
+++ Assets/Scripts/SimpleCameraFollow.cs
@@ -17,7 +17,6 @@ public class SimpleCameraFollow : MonoBehaviour
     Vector3 direction; // Направление от снаряда до цели
     GameObject directionSphere; //Обоъект для создания шаблона префаба
     Material directionSphereMaterial; //Материал сферы направления
-    Color saveBaseHDRColor; //Цвет, который сохраняет начальные параметры цвета материала, чтобы потом от них отталкиваться
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +24,6 @@ public class SimpleCameraFollow : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Target");
         directionSphere = Instantiate(directionSpherePrefab); //Создаем шаблон префаба в игре
         directionSphereMaterial = directionSphere.GetComponent<MeshRenderer>().materials[0];
-        saveBaseHDRColor = directionSphereMaterial.GetColor("_GlowColor");
         directionSphere.SetActive(false);
     }
 
@@ -41,16 +39,15 @@ public class SimpleCameraFollow : MonoBehaviour
         }
         LastPos = player.transform.position;
 
+        SetDirection();    
+    }
 
-
-
-
-
-        // Код для указания направления до цели
+    public void SetDirection() //Метод, отвечающий за создание направляющей сферы до цели
+    {
         planes = GeometryUtility.CalculateFrustumPlanes(Camera.main); //Заполняем массив плоскостей камеры
 
-        if (!GeometryUtility.TestPlanesAABB(planes,target.GetComponent<SphereCollider>().bounds) && GeometryUtility.TestPlanesAABB(planes, player.GetComponent<SphereCollider>().bounds) && showDirection) //Если цель не находится в зоне видимости (ОПИРАЕМСЯ НА КОЛЛАЙДЕР ЦЕЛИ)
-        { 
+        if (!GeometryUtility.TestPlanesAABB(planes, target.GetComponent<SphereCollider>().bounds) && GeometryUtility.TestPlanesAABB(planes, player.GetComponent<SphereCollider>().bounds) && showDirection) //Если цель не находится в зоне видимости (ОПИРАЕМСЯ НА КОЛЛАЙДЕР ЦЕЛИ)
+        {
             directionSphere.SetActive(true);
 
             direction = target.transform.position - player.transform.position; //Находим направление от снаряда к цели
@@ -60,19 +57,20 @@ public class SimpleCameraFollow : MonoBehaviour
             {
                 planes[i].Raycast(ray, out distance);
                 if (distance < distancceMin && distance > 0) //Если дистанция до другой плоскости меньше, чем до заранее найденной - выбираем его
-                    distancceMin = distance+0.0185f; // 0.0185 - это подобранное значение, на которое сфера сдвигается от снаряда за рамку камеры
+                    distancceMin = distance + 0.0185f; // 0.0185 - это подобранное значение, на которое сфера сдвигается от снаряда за рамку камеры
             }
 
             directionSphere.transform.position = direction.normalized * distancceMin + player.transform.position; //Перемещаем сферу направления в точку пересечения с ближайшей плоскостью (не забывая, что со сдвигом в 0,0185) 
-            directionSphere.transform.localScale = new Vector3(Camera.main.transform.position.x / 7, Camera.main.transform.position.x / 7, Camera.main.transform.position.x / 7) *0.05f; // Деление на 7 - это подобранный параметр, т.к. все настраивалось относительно положения камеры от плоскости YZ на расстоянии 7, а умножение на 0,05 - это возврат к базовому размеру шарика, тоже подобранный параметр
+            directionSphere.transform.localScale = new Vector3(Camera.main.transform.position.x / 7, Camera.main.transform.position.x / 7, Camera.main.transform.position.x / 7) * 0.05f; // Деление на 7 - это подобранный параметр, т.к. все настраивалось относительно положения камеры от плоскости YZ на расстоянии 7, а умножение на 0,05 - это возврат к базовому размеру шарика, тоже подобранный параметр
             // Строчка выше также нужна, чтобы размер directionSphere менялся в зависимости от удаления камеры
 
-            directionSphereMaterial.SetColor("_GlowColor", target.GetComponent<MeshRenderer>().materials[0].GetColor("_GlowColor")* 6 * ChangeColorByVelocity.Sigmoid((direction.magnitude- setDecayInteisityDistance) *10)); //Изменение свечения материала сферы направления по измененной сигмоиде. Функция сигмоиды определена в самописном скрипте. Умножение на 6 нужно для того, чтобы компенсировать слабое сияние цвета материала цели
-            
+            directionSphereMaterial.SetColor("_GlowColor", target.GetComponent<MeshRenderer>().materials[0].GetColor("_GlowColor") * 6 * ChangeColorByVelocity.Sigmoid((direction.magnitude - setDecayInteisityDistance) * 10)); //Изменение свечения материала сферы направления по измененной сигмоиде. Функция сигмоиды определена в самописном скрипте. Умножение на 6 нужно для того, чтобы компенсировать слабое сияние цвета материала цели
+
             distancceMin = 10000000; //Устанавливаем немозможное большое значение, чтобы со следующего кадра все было ок
         }
         else
             directionSphere.SetActive(false); // Если цель видна - то выключаем подсветку
     }
+
 }
 
