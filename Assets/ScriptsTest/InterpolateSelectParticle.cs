60139b6dca51d50100d5d20e385a7e9a96789d4a
Mikhail <tokkotilo@mail.ru>
2020-04-11 21:21:14 +0300

    Added SelectParticle and script with them inside

---
+++ Assets/ScriptsTest/InterpolateSelectParticle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterpolateSelectParticle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool IsClosest;
+
+    ParticleSystem.ShapeModule pShape;
+    
+    void Start()
+    {
+        pShape = GameObject.Find("SelectParticle").GetComponent<ParticleSystem>().shape;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsClosest)
+        {
+            if (pShape.radius <= 2)
+                pShape.radius +=Time.deltaTime*10;
+        }
+        else
+        {
+            pShape.radius -= Time.deltaTime * 10;
+        }
+    }
+}
